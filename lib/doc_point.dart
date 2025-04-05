29534094265f05574fef78e8d84d6fa2b990e0c3
shroida <shroida012@gmail.com>
2025-04-05 12:55:43 +0200

    readd packages

--- lib/doc_point.dart
+++ lib/doc_point.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocPoint extends StatelessWidget {
   const DocPoint({super.key, required this.appRouter});
@@ -7,11 +8,12 @@ class DocPoint extends StatelessWidget {
   final GoRouter appRouter;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: appRouter.routeInformationParser,
-      routerDelegate: appRouter.routerDelegate,
-      routeInformationProvider: appRouter.routeInformationProvider,
-      debugShowCheckedModeBanner: false,
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          
+        });
   }
 }
