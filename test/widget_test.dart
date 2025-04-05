da0425ce7a7c0de022735e93cfb660cf4c8c0348
shroida <shroida012@gmail.com>
2025-04-05 11:17:13 +0200

    proive router in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:docpoint/core/routing/app_router.dart';
 import 'package:docpoint/doc_point.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -12,7 +13,10 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const DocPoint());
+    final appRouter = AppRouter();
+    await tester.pumpWidget(DocPoint(
+      appRouter: appRouter.router,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
