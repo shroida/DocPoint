98f46959e38356ca3fd2b213809369a8e2f46c55
shroida <shroida012@gmail.com>
2025-04-05 14:11:58 +0200

    add text

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -5,7 +5,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return const SafeArea(
       child: Text('Log in'),
     );
   }
