5c4756896e7312939c94be93a42353668b61dbfc
shroida <shroida012@gmail.com>
2025-04-05 13:10:24 +0200

    solve  the typo

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:docpoint/core/routing/routes.dart';
-import 'package:docpoint/features/onboadring/onboadring_screen.dart';
+import 'package:docpoint/features/onboadring/onboarding_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
