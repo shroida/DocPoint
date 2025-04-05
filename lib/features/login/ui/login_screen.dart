1629151b531068c637c6e91f3d2718e49b7d7cda
shroida <shroida012@gmail.com>
2025-04-05 13:53:42 +0200

    navigate to login

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -5,8 +5,9 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return const Scaffold(
+        body: Center(
       child: Text('Log in'),
-    );
+    ));
   }
 }
