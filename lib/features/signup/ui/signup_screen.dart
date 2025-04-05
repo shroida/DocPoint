fca88d1f1477007ec766568bbbf07e5c1f755749
shroida <shroida012@gmail.com>
2025-04-05 14:47:31 +0200

    add some stuff

--- lib/features/signup/ui/signup_screen.dart
+++ lib/features/signup/ui/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:location/location.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -8,13 +10,39 @@ class SignupScreen extends StatefulWidget {
 }
 
 class SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String userType = 'Patient';
+  String email = '';
+  String password = '';
+  String phoneNumber = '';
+  String firstName = '';
+  String lastName = '';
+  String city = 'Guwahati';
+  String profileImageUrl = '';
+  String category = 'Dentist';
+  String qualification = '';
+  String yearsOfExperience = '';
+  double latitude = 0.0;
+  double longitude = 0.0;
+  final ImagePicker _picker = ImagePicker();
+  XFile? _imageFile;
+
+  final Location _location = Location();
+  bool _isLoading = false;
+  bool _obscureText = true;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold( 
-        body: SafeArea(
-      child: Center(
-        child: Text('signup'),
-      ),
-    ));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Register',
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+        ),
+        body: const SafeArea(
+          child: Center(
+            child: Text('signup'),
+          ),
+        ));
   }
 }
