5d33b16e252b82ecda8a02081e2f5e83f1a072fc
shroida <shroida012@gmail.com>
2025-04-05 14:14:11 +0200

    SignupScreen init

---
+++ lib/features/signup/ui/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  SignupScreenState createState() => SignupScreenState();
+}
+
+class SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
