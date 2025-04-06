fbb08276460c81eb8e43c76f8b32e49578930523
shroida <shroida012@gmail.com>
2025-04-06 18:37:02 +0200

    init states & cubit

---
+++ lib/features/signup/presentation/logic/cubit/signup_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'signup_state.dart';
+
+class SignupCubit extends Cubit<SignupState> {
+  SignupCubit() : super(SignupInit());
+}
---
+++ lib/features/signup/presentation/logic/cubit/signup_state.dart
@@ -0,0 +1,9 @@
+part of 'signup_cubit.dart';
+
+class SignupState {}
+
+class SignupInit extends SignupState {}
+
+class SignupSuccess extends SignupState {}
+
+class SignupFailure extends SignupState {}
