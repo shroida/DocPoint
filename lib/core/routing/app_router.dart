45e7e9bfea89828a4a1230fec9f6600d6f983663
shroida <shroida012@gmail.com>
2025-04-06 20:19:04 +0200

    wrap SignupScreen by SignupCubit

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:docpoint/core/routing/routes.dart';
 import 'package:docpoint/features/login/ui/login_screen.dart';
 import 'package:docpoint/features/onboadring/onboarding_screen.dart';
+import 'package:docpoint/features/signup/presentation/logic/cubit/signup_cubit.dart';
 import 'package:docpoint/features/signup/presentation/ui/signup_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -16,7 +18,10 @@ class AppRouter {
           builder: (context, state) => const LoginScreen()),
       GoRoute(
           path: Routes.signupScreen,
-          builder: (context, state) => const SignupScreen()),
+          builder: (context, state) => BlocProvider(
+                create: (context) => SignupCubit(),
+                child: const SignupScreen(),
+              )),
     ],
     initialLocation: Routes.onBoardingScreen,
   );
