c889af93b374bf9aa5430c99cae1cda28f495b53
shroida <shroida012@gmail.com>
2025-04-06 19:08:50 +0200

    add controllers

--- lib/features/signup/presentation/logic/cubit/signup_cubit.dart
+++ lib/features/signup/presentation/logic/cubit/signup_cubit.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'signup_state.dart';
 
 class SignupCubit extends Cubit<SignupState> {
   SignupCubit() : super(SignupInit());
-  
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController firstController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController experienceController = TextEditingController();
 }
