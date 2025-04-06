1c1ace55e78cc41f5305623b184e49719638b0a4
shroida <shroida012@gmail.com>
2025-04-06 22:53:52 +0200

    apply in dropdown

--- lib/core/widgets/app_dropdown_form_field.dart
+++ lib/core/widgets/app_dropdown_form_field.dart
@@ -1,5 +1,6 @@
+import 'package:docpoint/core/styles/app_colors.dart';
+import 'package:docpoint/core/styles/app_styles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppDropdownFormField<T> extends StatelessWidget {
   final EdgeInsetsGeometry? contentPadding;
@@ -33,61 +34,21 @@ class AppDropdownFormField<T> extends StatelessWidget {
   Widget build(BuildContext context) {
     return DropdownButtonFormField<T>(
       value: value,
-      decoration: InputDecoration(
-        isDense: true,
-        contentPadding: contentPadding ??
-            EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-        focusedBorder: focusedBorder ??
-            OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Color(0xFF0064FA),
-                width: 1.3,
-              ),
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-        enabledBorder: enabledBorder ??
-            OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: Color(0xFF9E9E9E),
-                width: 1.3,
-              ),
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-        errorBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: Colors.red,
-            width: 1.3,
-          ),
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: const BorderSide(
-            color: Colors.red,
-            width: 1.3,
-          ),
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        hintStyle: hintStyle ??
-            const TextStyle(
-              fontSize: 14,
-              color: Color(0xFF9E9E9E),
-              fontWeight: FontWeight.normal,
-            ),
+      decoration: AppStyle.inputDecoration(
         hintText: hintText,
-        fillColor: backgroundColor ?? const Color(0xFFF5F5F5),
-        filled: true,
+        contentPadding: contentPadding,
+      ).copyWith(
+        focusedBorder: focusedBorder,
+        enabledBorder: enabledBorder,
+        hintStyle: hintStyle,
+        fillColor: backgroundColor,
       ),
       items: items,
       onChanged: onChanged,
-      style: inputTextStyle ??
-          const TextStyle(
-            fontSize: 14,
-            color: Color(0xFF0D1F3C),
-            fontWeight: FontWeight.w500,
-          ),
+      style: inputTextStyle ?? AppStyle.body2,
       validator: validator != null ? (value) => validator!(value) : null,
-      icon: const Icon(Icons.arrow_drop_down, color: Color(0xFF0D1F3C)),
-      dropdownColor: backgroundColor ?? const Color(0xFFF5F5F5),
+      icon: const Icon(Icons.arrow_drop_down, color: AppColors.textPrimary),
+      dropdownColor: backgroundColor ?? AppColors.background,
       borderRadius: BorderRadius.circular(16.0),
       isExpanded: true,
     );
