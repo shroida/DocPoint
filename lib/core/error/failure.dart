954caf3a59d91eadc1e35a8ef14c02ea68941070
shroida <shroida012@gmail.com>
2025-04-07 10:53:06 +0200

    error file

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,16 @@
+abstract class Failure {
+  final String message;
+
+  const Failure(this.message);
+
+  @override
+  String toString() => message;
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  const NetworkFailure() : super('No internet connection');
+}
