a0cd9fb926e165a8f53d7dac4945a7fb4c7055c9
shroida <shroida012@gmail.com>
2025-04-07 15:12:44 +0200

    LoginRemoteDatasource

---
+++ lib/features/login/data/datasources/login_remote_datasource.dart
@@ -0,0 +1,38 @@
+import 'package:docpoint/core/error/failure.dart';
+import 'package:docpoint/features/login/data/models/user_login_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class LoginRemoteDatasource {
+  Future<UserLoginModel> login(
+      {required String email,
+      required String password,
+      required String firstName,
+      required String lastName,
+      required String phoneNumber,
+      required String city});
+}
+
+class LoginRemoteDatasourceImpl implements LoginRemoteDatasource {
+  final SupabaseClient supabaseClient;
+
+  LoginRemoteDatasourceImpl(this.supabaseClient);
+  @override
+  Future<UserLoginModel> login(
+      {required String email,
+      required String password,
+      required String firstName,
+      required String lastName,
+      required String phoneNumber,
+      required String city}) async {
+    final resppnse = await supabaseClient.auth
+        .signUp(email: email, password: password, data: {
+      'first_name': firstName,
+      'last_name': lastName,
+      'phone_number': phoneNumber,
+      'city': city,
+    });
+    if (resppnse.user == null) {
+      throw Failure('error');
+    }
+  }
+}
