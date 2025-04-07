4a68b7542a55a213fbd281b8ec4e9b9777e05479
shroida <shroida012@gmail.com>
2025-04-07 12:19:52 +0200

    app constatns url

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:docpoint/core/constants/constants.dart';
 import 'package:docpoint/core/routing/app_router.dart';
 import 'package:docpoint/doc_point.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(url: projectURL, anonKey: anonKey);
+
   final appRouter = AppRouter().router;
   runApp(DocPoint(
     appRouter: appRouter,
