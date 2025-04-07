2d0f5c1003e33b32b15757968828cdcd9093d641
shroida <shroida012@gmail.com>
2025-04-07 11:19:41 +0200

    current user usecase done

---
+++ lib/core/common/domain/usecase/current_user_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:docpoint/core/common/domain/entites/user.dart';
+import 'package:docpoint/core/common/domain/repository/current_user_repo.dart';
+import 'package:docpoint/core/error/failure.dart';
+import 'package:docpoint/core/usecase/usecase.dart';
+import 'package:fpdart/fpdart.dart';
+
+class CurrentUserUsecase implements UseCase<User, NoParams> {
+  final CurrentUserRepo _currentUserRepo;
+
+  CurrentUserUsecase(this._currentUserRepo);
+
+  @override
+  Future<Either<Failure, User>> call(NoParams params) {
+    return _currentUserRepo.currentUser();
+  }
+}
