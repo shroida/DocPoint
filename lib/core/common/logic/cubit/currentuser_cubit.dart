166841be10141168ab9bb9f75fd53f8d122be369
shroida <shroida012@gmail.com>
2025-04-07 11:29:18 +0200

    current user cubit

--- lib/core/common/logic/cubit/currentuser_cubit.dart
+++ lib/core/common/logic/cubit/currentuser_cubit.dart
@@ -1,6 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:docpoint/core/common/domain/usecase/current_user_usecase.dart';
 import 'package:docpoint/core/common/logic/cubit/current_user_state.dart';
+import 'package:docpoint/core/usecase/usecase.dart';
 
 class CurrentuserCubit extends Cubit<CurrentUserState> {
-  CurrentuserCubit() : super(CurrentUserInitial());
+  final CurrentUserUsecase _currentUserUsecase;
+  CurrentuserCubit(this._currentUserUsecase) : super(CurrentUserInitial());
+
+  Future<void> isUserLoggedIn() async {
+    final res = await _currentUserUsecase.call(NoParams());
+    res.fold((failure) => emit(CurrentUserInitial()),
+        (user) => emit(CurrentUserLoggedIn(user)));
+  }
 }
