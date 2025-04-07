22427bf45f2931b9856aa3cbe58221dc7c4c160d
shroida <shroida012@gmail.com>
2025-04-07 10:47:25 +0200

    init use r entity

---
+++ lib/core/common/domain/entites/user.dart
@@ -0,0 +1,16 @@
+class User {
+  final String email;
+  final String password;
+  final String firstName;
+  final String lastName;
+  final String phoneNumber;
+  final String city;
+
+  User(
+      {required this.email,
+      required this.password,
+      required this.firstName,
+      required this.lastName,
+      required this.phoneNumber,
+      required this.city});
+}
