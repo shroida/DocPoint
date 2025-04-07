c0a332b1b0f51d8962502b17a535ba57e5a5cdc5
shroida <shroida012@gmail.com>
2025-04-07 15:43:59 +0200

    SignUpRepoImpl

---
+++ lib/features/signup/data/repositories/sign_up_repo_impl.dart
@@ -0,0 +1,34 @@
+import 'package:docpoint/core/common/domain/entites/user.dart';
+import 'package:docpoint/core/error/failure.dart';
+import 'package:docpoint/core/error/server_exeptions.dart';
+import 'package:docpoint/features/signup/data/datasources/sign_up_remote_datasource.dart';
+import 'package:docpoint/features/signup/domain/repository/sign_up_repo.dart';
+import 'package:fpdart/fpdart.dart';
+
+class SignUpRepoImpl implements SignUpRepo {
+  final SignUpRemoteDatasource _signUpRemoteDatasource;
+
+  SignUpRepoImpl(this._signUpRemoteDatasource);
+  @override
+  Future<Either<Failure, User>> signUp(
+      {required String email,
+      required String password,
+      required String firstName,
+      required String lastName,
+      required String phoneNumber,
+      required String city}) async {
+    try {
+      final user = await _signUpRemoteDatasource.signUp(
+          email: email,
+          password: password,
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+          city: city);
+
+      return right(user);
+    } on ServerExceptions catch (e) {
+      return left(ServerFailure(e.message));
+    }
+  }
+}
