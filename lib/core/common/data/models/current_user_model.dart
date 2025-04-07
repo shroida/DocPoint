60e763c84c428321cbbb9d58a696b32d7ca8b2c3
shroida <shroida012@gmail.com>
2025-04-07 11:50:33 +0200

    CurrentUserModel

---
+++ lib/core/common/data/models/current_user_model.dart
@@ -0,0 +1,22 @@
+import 'package:docpoint/core/common/domain/entites/user.dart';
+
+class CurrentUserModel extends User {
+  CurrentUserModel(
+      {required super.email,
+      required super.password,
+      required super.firstName,
+      required super.lastName,
+      required super.phoneNumber,
+      required super.city});
+
+  factory CurrentUserModel.fromJson(Map<String, dynamic> json) {
+    return CurrentUserModel(
+        email: json['email'],
+        password: json['password'],
+        firstName: json['first_name'],
+        lastName: json['last_name'],
+        phoneNumber: json['phone_number'],
+        city: json['city']);
+  }
+  
+}
