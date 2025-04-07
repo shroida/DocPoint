f42df55976849b722e0d4a5e0359af8d0df3319e
shroida <shroida012@gmail.com>
2025-04-07 12:20:51 +0200

    WidgetsFlutterBinding

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(url: projectURL, anonKey: anonKey);
 
   final appRouter = AppRouter().router;
