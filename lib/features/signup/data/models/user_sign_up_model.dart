5dc36dff89134c3511001798990ac74df1a72d04
shroida <shroida012@gmail.com>
2025-04-09 17:12:52 +0200

    UserSignUpModel from json

--- lib/features/signup/data/models/user_sign_up_model.dart
+++ lib/features/signup/data/models/user_sign_up_model.dart
@@ -1,21 +1,32 @@
 import 'package:docpoint/features/signup/domain/entities/user_sign_up.dart';
 
 class UserSignUpModel extends UserSignUp {
-  UserSignUpModel(
-      {required super.email,
-      required super.password,
-      required super.firstName,
-      required super.lastName,
-      required super.phoneNumber,
-      required super.city});
+  UserSignUpModel({
+    required super.email,
+    required super.password,
+    required super.firstName,
+    required String super.lastName,
+    required String super.phoneNumber,
+    required String super.city,
+  });
 
   factory UserSignUpModel.fromJson(Map<String, dynamic> json) {
     return UserSignUpModel(
-        email: json['email'],
-        password: json['password'],
-        firstName: json['firstName'],
-        lastName: json['lastName'],
-        phoneNumber: json['phoneNumber'],
-        city: json['city']);
+      email: json['email'] as String? ?? '',
+      password: json['password'] as String? ?? '',
+      firstName: json['firstName'] as String? ?? '',
+      lastName: json['lastName'] as String? ?? '',
+      phoneNumber: json['phoneNumber'] as String? ?? '',
+      city: json['city'] as String? ?? '',
+    );
   }
+
+  Map<String, dynamic> toJson() => {
+        'email': email,
+        'password': password,
+        'firstName': firstName,
+        'lastName': lastName,
+        'phoneNumber': phoneNumber,
+        'city': city,
+      };
 }
