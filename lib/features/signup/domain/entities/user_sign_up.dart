a2a18905c2a9bb54dd8c72a682482ce068bf0403
shroida <shroida012@gmail.com>
2025-04-10 16:05:49 +0200

    add experience & category in signup entity

--- lib/features/signup/domain/entities/user_sign_up.dart
+++ lib/features/signup/domain/entities/user_sign_up.dart
@@ -5,7 +5,9 @@ class UserSignUp extends User {
       {required super.email,
       required super.password,
       required super.firstName,
-      super.imageUrl,
+      required super.category,
+      required super.experience,
+      required super.imageUrl,
       required super.lastName,
       required super.phoneNumber,
       required super.city});
