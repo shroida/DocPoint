a9c92283c52bef92d55735e98c319a9b63f1dd8c
shroida <shroida012@gmail.com>
2025-04-11 15:31:39 +0200

    show data

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,10 +1,28 @@
+import 'package:docpoint/core/common/logic/cubit/current_user_state.dart';
+import 'package:docpoint/core/common/logic/cubit/currentuser_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('tesdfas')));
+    return Scaffold(
+      body: BlocBuilder<CurrentUserCubit, CurrentUserState>(
+        builder: (context, state) {
+          if (state is CurrentUserLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is CurrentUserAuthenticated) {
+            return Center(child: Text('Welcome ${state.user.city}'));
+          } else if (state is CurrentUserUnauthenticated) {
+            return const Center(child: Text('Not logged in'));
+          } else if (state is CurrentUserError) {
+            return Center(child: Text('Error: ${state.message}'));
+          }
+          return const Center(child: Text('Checking auth...'));
+        },
+      ),
+    );
   }
 }
