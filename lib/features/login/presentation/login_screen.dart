538e50f6c7947e509d335ea3d127cc2bf4beaf61
shroida <shroida012@gmail.com>
2025-04-13 09:38:26 +0200

    Form & text fields

--- lib/features/login/presentation/login_screen.dart
+++ lib/features/login/presentation/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:docpoint/core/widgets/app_text_form_field.dart';
 import 'package:docpoint/features/login/presentation/widgets/logo_login.dart';
 import 'package:flutter/material.dart';
 
@@ -20,13 +21,39 @@ class LoginScreenState extends State<LoginScreen> {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
         ),
       ),
-      body: const SafeArea(
+      body:  SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                 LogoLogin(),
+                 const LogoLogin(),
+                 Form(child: Column(
+                  children: [
+                    AppTextFormField(
+                      controller: ,
+                      hintText: 'Email',
+                      validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email';
+              }
+              return null;
+            },
+                    ),
+                    SizedBox(height: 10.h),
+          AppTextFormField(
+            hintText: 'Password',
+            controller: ,
+            isObscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+                  ],
+                 ))
                 ],
               )),
         ),
