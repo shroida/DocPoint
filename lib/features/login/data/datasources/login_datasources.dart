c395d09427f8e242968acd5c28c9b9551e05c586
shroida <shroida012@gmail.com>
2025-04-13 11:22:18 +0200

    provide CurrentUserCubit in login datasources

--- lib/features/login/data/datasources/login_datasources.dart
+++ lib/features/login/data/datasources/login_datasources.dart
@@ -1,3 +1,4 @@
+import 'package:docpoint/core/common/logic/cubit/currentuser_cubit.dart';
 import 'package:docpoint/core/error/server_exeptions.dart';
 import 'package:docpoint/features/login/data/models/user_login_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -9,8 +10,8 @@ abstract interface class LoginDatasources {
 
 class LoginDatasourcesImpl implements LoginDatasources {
   final SupabaseClient _supabaseClient;
-
-  LoginDatasourcesImpl(this._supabaseClient);
+  final CurrentUserCubit currentUserCubit;
+  LoginDatasourcesImpl(this._supabaseClient, this.currentUserCubit);
   @override
   Future<UserLoginModel> login(
       {required String email, required String password}) async {
@@ -18,7 +19,6 @@ class LoginDatasourcesImpl implements LoginDatasources {
       final response = await _supabaseClient.auth
           .signInWithPassword(email: email, password: password);
       final userId = response.user!.id;
-      
     } catch (e) {
       throw const ServerExceptions('Error Login');
     }
