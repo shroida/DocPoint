99826b926c6d6bfd068cece12a9ea15e2a787ee0
shroida <shroida012@gmail.com>
2025-04-13 10:43:27 +0200

    LoginRepo interface

---
+++ lib/features/login/domain/repository/login_repo.dart
@@ -0,0 +1,8 @@
+import 'package:docpoint/core/common/domain/entites/user.dart';
+import 'package:docpoint/core/error/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class LoginRepo {
+  Future<Either<Failure, User>> login(
+      {required String email, required String password});
+}
