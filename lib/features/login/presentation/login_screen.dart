5c6dc4077d7b96f29a77f35b1d3270a511b8846a
shroida <shroida012@gmail.com>
2025-04-13 09:15:44 +0200

    remove import

--- lib/features/login/presentation/login_screen.dart
+++ lib/features/login/presentation/login_screen.dart
@@ -1,14 +1,4 @@
-import 'package:docpoint/core/styles/app_styles.dart';
-import 'package:docpoint/core/widgets/app_text_button.dart';
-import 'package:docpoint/features/signup/presentation/logic/cubit/signup_cubit.dart';
-import 'package:docpoint/features/signup/presentation/logic/cubit/signup_state.dart';
-import 'package:docpoint/features/signup/presentation/ui/widgets/doctor_form.dart';
-import 'package:docpoint/features/signup/presentation/ui/widgets/patient_form.dart';
-import 'package:docpoint/features/signup/presentation/ui/widgets/pick_image.dart';
-import 'package:docpoint/features/signup/presentation/ui/widgets/user_type_selector.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
