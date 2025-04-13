b8e6e88e20be7a073be7f18974c2bbd72ee65a1b
shroida <shroida012@gmail.com>
2025-04-13 09:45:07 +0200

    controllers & form key

--- lib/features/login/presentation/logic/login_cubit.dart
+++ lib/features/login/presentation/logic/login_cubit.dart
@@ -1,8 +1,13 @@
-
-
 import 'package:docpoint/features/login/presentation/logic/login_state.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
+  // Form key for login Form
+  final formKey = GlobalKey<FormState>();
+
+  // Login controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   LoginCubit() : super(LoginInitial());
 }
