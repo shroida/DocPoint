f4398a837950263d82caf57be368af15443b50db
shroida <shroida012@gmail.com>
2025-04-13 09:46:47 +0200

    call controllers from cubit

--- lib/features/login/presentation/login_screen.dart
+++ lib/features/login/presentation/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:docpoint/core/widgets/app_text_form_field.dart';
+import 'package:docpoint/features/login/presentation/logic/login_cubit.dart';
 import 'package:docpoint/features/login/presentation/widgets/logo_login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,6 +17,7 @@ class LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final loginCubit = context.read<LoginCubit>();
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -21,39 +25,41 @@ class LoginScreenState extends State<LoginScreen> {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
         ),
       ),
-      body:  SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                 const LogoLogin(),
-                 Form(child: Column(
-                  children: [
-                    AppTextFormField(
-                      controller: ,
-                      hintText: 'Email',
-                      validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your email';
-              }
-              return null;
-            },
-                    ),
-                    SizedBox(height: 10.h),
-          AppTextFormField(
-            hintText: 'Password',
-            controller: ,
-            isObscureText: true,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              return null;
-            },
-          ),
-                  ],
-                 ))
+                  const LogoLogin(),
+                  Form(
+                      key: loginCubit.formKey,
+                      child: Column(
+                        children: [
+                          AppTextFormField(
+                            controller: loginCubit.emailController,
+                            hintText: 'Email',
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 10.h),
+                          AppTextFormField(
+                            hintText: 'Password',
+                            controller: loginCubit.passwordController,
+                            isObscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ))
                 ],
               )),
         ),
