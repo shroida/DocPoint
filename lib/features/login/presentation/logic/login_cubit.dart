fb1e75a2f081ef1c912571398bbedb78126f25b2
shroida <shroida012@gmail.com>
2025-04-13 09:41:46 +0200

    init login cubit & states

---
+++ lib/features/login/presentation/logic/login_cubit.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:docpoint/features/login/presentation/logic/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+}
