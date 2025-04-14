74a48ba06ab37bad1945150e9848dc2544a9ad12
shroida <shroida012@gmail.com>
2025-04-14 08:56:50 +0200

    login success

--- lib/features/login/data/datasources/login_datasources.dart
+++ lib/features/login/data/datasources/login_datasources.dart
@@ -2,6 +2,7 @@ import 'package:docpoint/core/common/data/models/current_user_model.dart';
 import 'package:docpoint/core/common/logic/cubit/currentuser_cubit.dart';
 import 'package:docpoint/core/error/server_exeptions.dart';
 import 'package:docpoint/features/login/data/models/user_login_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class LoginDatasources {
@@ -17,11 +18,16 @@ class LoginDatasourcesImpl implements LoginDatasources {
   Future<UserLoginModel> login(
       {required String email, required String password}) async {
     try {
+      debugPrint('================DATASOURCES==========================');
+      debugPrint('Logging in with email: $email and password: $password');
       // 1. Authenticate with Supabase
       final response = await _supabaseClient.auth
           .signInWithPassword(email: email, password: password);
+
       final user = response.user!;
       final userId = user.id;
+      debugPrint('================DATASOURCES==========================');
+      debugPrint('Logging in with userID: $userId ==========');
       // 2. Fetch user profile based on user type
       final profileData = await _fetchUserProfile(userId, user.email!);
 
@@ -35,17 +41,21 @@ class LoginDatasourcesImpl implements LoginDatasources {
         }).toUserEntity();
         currentUserCubit.updateUser(currentUser);
       }
+      debugPrint('================DATASOURCES==========================');
+      debugPrint('Logging in with userData: $userId ==========');
       return UserLoginModel(email: email, password: password, id: userId);
     } catch (e) {
+      debugPrint('Login error: $e'); // Log the actual error
       throw const ServerExceptions('Error Login');
     }
   }
 
   Future<Map<String, dynamic>> _fetchUserProfile(
       String userId, String email) async {
-    // Check if user exists in profiles table
     final profileResponse = await _supabaseClient
-        .from('user_profiles')
+        .from(currentUserCubit.userType == 'Doctor'
+            ? 'doctor_profiles'
+            : 'patient_profiles')
         .select()
         .eq('id', userId)
         .maybeSingle();
