dec2aae2344d3ea2b57fd210f4b83df9dc546fe8
shroida <shroida012@gmail.com>
2025-04-14 19:27:51 +0200

    states

--- lib/features/home/presentation/logic/home_page_state.dart
+++ lib/features/home/presentation/logic/home_page_state.dart
@@ -1,3 +1,19 @@
-class HomePageState {}
+import 'package:docpoint/features/home/domain/entities/doctor_entity.dart';
+
+abstract class HomePageState {}
 
 final class HomePageInitial extends HomePageState {}
+
+final class HomePageLoading extends HomePageState {}
+
+final class HomePageLoaded extends HomePageState {
+  final List<DoctorEntity> doctors;
+
+  HomePageLoaded(this.doctors);
+}
+
+final class HomePageError extends HomePageState {
+  final String message;
+
+  HomePageError(this.message);
+}
