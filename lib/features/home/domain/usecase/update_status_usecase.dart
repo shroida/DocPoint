ea678f47ad171da94f011b425fde499d5b70f124
shroida <shroida012@gmail.com>
2025-04-15 22:22:34 +0200

    UpdateStatusUsecase done

--- lib/features/home/domain/usecase/update_status_usecase.dart
+++ lib/features/home/domain/usecase/update_status_usecase.dart
@@ -1,11 +1,13 @@
 import 'package:docpoint/core/error/failure.dart';
 import 'package:docpoint/core/usecase/usecase.dart';
+import 'package:docpoint/features/home/domain/repositories/doctors_repo.dart';
 import 'package:fpdart/fpdart.dart';
 
 class UpdateStatusUsecase implements UseCase<void, UpdateStatusParams> {
+  late final GetAllDoctorsRepo _getAllDoctorsRepo;
   @override
-  Future<Either<Failure, void>> call(UpdateStatusParams params) {
-    
+  Future<Either<Failure, void>> call(UpdateStatusParams params) async {
+    return await _getAllDoctorsRepo.updateStatusAppointment();
   }
 }
 
