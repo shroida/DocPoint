bbc21b4b246f7baf0e64be0cc89a230d3972fd34
shroida <shroida012@gmail.com>
2025-04-15 09:48:12 +0200

    make appointment routes

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,5 +2,6 @@ class Routes {
   static const String onBoardingScreen = '/onBoardingScreen';
   static const String loginScreen = '/loginScreen';
   static const String signupScreen = '/signupScreen';
+  static const String makeAppointment = '/makeAppointment';
   static const String homePage = '/homePage';
 }
