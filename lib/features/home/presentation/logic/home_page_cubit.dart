d560db72c2f65504384f6a478ac3bf6580329951
shroida <shroida012@gmail.com>
2025-04-15 22:29:05 +0200

    updateStatusAppointment in cubit

--- lib/features/home/presentation/logic/home_page_cubit.dart
+++ lib/features/home/presentation/logic/home_page_cubit.dart
@@ -5,6 +5,7 @@ import 'package:docpoint/features/home/domain/entities/doctor_entity.dart';
 import 'package:docpoint/features/home/domain/usecase/get_all_appointments.dart';
 import 'package:docpoint/features/home/domain/usecase/get_all_doctors.dart';
 import 'package:docpoint/features/home/domain/usecase/make_appointment.dart';
+import 'package:docpoint/features/home/domain/usecase/update_status_usecase.dart';
 import 'package:docpoint/features/home/presentation/logic/home_page_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,12 +13,13 @@ class HomePageCubit extends Cubit<HomePageState> {
   final GetAllDoctors _getAllDoctors;
   final MakeAppointment _makeAppointment;
   final GetAllAppointments _getAllAppointments;
+  final UpdateStatusUsecase _updateStatusUsecase;
 
   List<DoctorEntity>? doctorList;
   List<AppointmentEntity>? appointmentList;
 
-  HomePageCubit(
-      this._getAllDoctors, this._makeAppointment, this._getAllAppointments)
+  HomePageCubit(this._getAllDoctors, this._makeAppointment,
+      this._getAllAppointments, this._updateStatusUsecase)
       : super(HomePageInitial());
 
   Future<void> getAllDoctors() async {
@@ -56,6 +58,17 @@ class HomePageCubit extends Cubit<HomePageState> {
       default:
         return 'Unexpected error: ${failure.message}';
     }
+  }
+
+  Future<void> updateStatusAppointment(
+      UpdateStatusParams updateStatusParams) async {
+    emit(AppointmentLoading());
+    final result = await _updateStatusUsecase.call(updateStatusParams);
+
+    result.fold(
+      (failure) => emit(AppointmentFailure(_mapFailureToMessage(failure))),
+      (_) => emit(AppointmentSuccess()),
+    );
   }
 
   Future<void> getAllAppointments(
