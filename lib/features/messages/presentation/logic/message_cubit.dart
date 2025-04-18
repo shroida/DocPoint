ff635a78fa1fe783223b1ea7025fde3a45384bf7
shroida <shroida012@gmail.com>
2025-04-18 11:20:50 +0200

    cubit & states

--- lib/features/messages/presentation/logic/message_cubit.dart
+++ lib/features/messages/presentation/logic/message_cubit.dart
@@ -1,12 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:docpoint/core/usecase/usecase.dart';
+import 'package:docpoint/features/messages/domain/entities/message.dart';
+import 'package:docpoint/features/messages/domain/usecase/get_all_messages_usecase.dart';
 import 'package:docpoint/features/messages/domain/usecase/send_message_usecase.dart';
 import 'package:docpoint/features/messages/presentation/logic/message_state.dart';
 import 'package:docpoint/core/error/failure.dart';
 
 class MessageCubit extends Cubit<MessageState> {
   final SendMessageUsecase _sendMessageUsecase;
+  final GetAllMessagesUsecase _getAllMessagesUsecase;
 
-  MessageCubit(this._sendMessageUsecase) : super(MessageInitial());
+  MessageCubit(this._sendMessageUsecase, this._getAllMessagesUsecase)
+      : super(MessageInitial());
 
   Future<void> sendMessage(SendMessageParams params) async {
     emit(MessageLoading());
@@ -14,6 +19,15 @@ class MessageCubit extends Cubit<MessageState> {
     result.fold(
       (Failure failure) => emit(MessageError(failure.message)),
       (_) => emit(MessageSent()),
+    );
+  }
+
+  Future<void> getAllMessages() async {
+    emit(MessageLoading());
+    final result = await _getAllMessagesUsecase(NoParams());
+    result.fold(
+      (Failure failure) => emit(MessageError(failure.message)),
+      (List<Message> messages) => emit(MessagesLoaded(messages)),
     );
   }
 }
--- lib/features/messages/presentation/logic/message_state.dart
+++ lib/features/messages/presentation/logic/message_state.dart
@@ -1,3 +1,5 @@
+import 'package:docpoint/features/messages/domain/entities/message.dart';
+
 sealed class MessageState {}
 
 final class MessageInitial extends MessageState {}
@@ -5,6 +7,11 @@ final class MessageInitial extends MessageState {}
 final class MessageLoading extends MessageState {}
 
 final class MessageSent extends MessageState {}
+
+final class MessagesLoaded extends MessageState {
+  List<Message> messages;
+  MessagesLoaded(this.messages);
+}
 
 final class MessageError extends MessageState {
   final String error;
