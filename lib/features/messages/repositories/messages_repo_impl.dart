f7bbf2fbf566791ae66e4f62f143c052a9957685
shroida <shroida012@gmail.com>
2025-04-18 06:16:30 +0200

    MessagesRepoImpl doene send message

---
+++ lib/features/messages/repositories/messages_repo_impl.dart
@@ -0,0 +1,33 @@
+import 'package:docpoint/core/error/failure.dart';
+import 'package:docpoint/features/messages/datasource/messages_datasource.dart';
+import 'package:docpoint/features/messages/domain/entities/message.dart';
+import 'package:docpoint/features/messages/domain/repositories/messages_repo.dart';
+import 'package:fpdart/fpdart.dart';
+
+class MessagesRepoImpl implements MessagesRepo {
+  final MessagesDatasource _messagesDatasource;
+
+  MessagesRepoImpl(this._messagesDatasource);
+  @override
+  Either<Failure, List<Message>> getAllMessages() {
+    // TODO: implement getAllMessages
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> sendMessage(
+      {required String senderId,
+      required String receiverId,
+      required String messageText,
+      required DateTime createdAt,
+      required bool isRead,
+      String? parentId}) async {
+    await _messagesDatasource.sendMessage(
+        senderId: senderId,
+        receiverId: receiverId,
+        messageText: messageText,
+        createdAt: createdAt,
+        isRead: isRead);
+    return const Right(null);
+  }
+}
