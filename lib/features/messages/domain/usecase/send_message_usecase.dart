65f3f4af6e775c7f801af778190a9c9726e127ee
shroida <shroida012@gmail.com>
2025-04-18 05:51:58 +0200

    SendMessageUsecase done

---
+++ lib/features/messages/domain/usecase/send_message_usecase.dart
@@ -0,0 +1,31 @@
+import 'package:docpoint/core/error/failure.dart';
+import 'package:docpoint/core/usecase/usecase.dart';
+import 'package:docpoint/features/messages/domain/repositories/messages_repo.dart';
+import 'package:fpdart/fpdart.dart';
+
+class SendMessageUsecase implements UseCase<void, SendMessageParams> {
+  final MessagesRepo _messagesRepo;
+
+  SendMessageUsecase(this._messagesRepo);
+  @override
+  Future<Either<Failure, void>> call(SendMessageParams params) async {
+    return await _messagesRepo.sendMessage();
+  }
+}
+
+class SendMessageParams {
+  final String senderId;
+  final String receiverId;
+  final String messageText;
+  final DateTime createdAt;
+  final bool isRead;
+  final String parentId;
+
+  SendMessageParams(
+      {required this.senderId,
+      required this.receiverId,
+      required this.messageText,
+      required this.createdAt,
+      required this.isRead,
+      required this.parentId});
+}
