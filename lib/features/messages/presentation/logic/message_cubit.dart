850a3d689a4a80286dde045d439907f14551869f
shroida <shroida012@gmail.com>
2025-04-18 06:27:39 +0200

    state & cubit done message

--- lib/features/messages/presentation/logic/message_cubit.dart
+++ lib/features/messages/presentation/logic/message_cubit.dart
@@ -1,6 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:docpoint/features/messages/domain/usecase/send_message_usecase.dart';
 import 'package:docpoint/features/messages/presentation/logic/message_state.dart';
+import 'package:docpoint/core/error/failure.dart';
 
 class MessageCubit extends Cubit<MessageState> {
-  MessageCubit() : super(MessageInitial());
+  final SendMessageUsecase _sendMessageUsecase;
+
+  MessageCubit(this._sendMessageUsecase) : super(MessageInitial());
+
+  Future<void> sendMessage(SendMessageParams params) async {
+    emit(MessageLoading());
+    final result = await _sendMessageUsecase(params);
+    result.fold(
+      (Failure failure) => emit(MessageError(failure.message)),
+      (_) => emit(MessageSent()),
+    );
+  }
 }
--- lib/features/messages/presentation/logic/message_state.dart
+++ lib/features/messages/presentation/logic/message_state.dart
@@ -1,3 +1,12 @@
 sealed class MessageState {}
 
 final class MessageInitial extends MessageState {}
+
+final class MessageLoading extends MessageState {}
+
+final class MessageSent extends MessageState {}
+
+final class MessageError extends MessageState {
+  final String error;
+  MessageError(this.error);
+}
