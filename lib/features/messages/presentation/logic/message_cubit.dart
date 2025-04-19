dd7771bed3cd9625bfeed0b2176ef1be7a774080
shroida <shroida012@gmail.com>
2025-04-19 13:11:26 +0200

    cubit done

--- lib/features/messages/presentation/logic/message_cubit.dart
+++ lib/features/messages/presentation/logic/message_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:docpoint/core/usecase/usecase.dart';
 import 'package:docpoint/features/messages/domain/entities/message.dart';
 import 'package:docpoint/features/messages/domain/usecase/get_all_messages_usecase.dart';
+import 'package:docpoint/features/messages/domain/usecase/make_messages_read.dart';
 import 'package:docpoint/features/messages/domain/usecase/send_message_usecase.dart';
 import 'package:docpoint/features/messages/presentation/logic/message_state.dart';
 import 'package:docpoint/core/error/failure.dart';
@@ -9,8 +10,10 @@ import 'package:docpoint/core/error/failure.dart';
 class MessageCubit extends Cubit<MessageState> {
   final SendMessageUsecase _sendMessageUsecase;
   final GetAllMessagesUsecase _getAllMessagesUsecase;
+  final MakeMessagesRead _makeMessagesRead;
 
-  MessageCubit(this._sendMessageUsecase, this._getAllMessagesUsecase)
+  MessageCubit(this._sendMessageUsecase, this._getAllMessagesUsecase,
+      this._makeMessagesRead)
       : super(MessageInitial());
 
   List<Message> allMessages = [];
@@ -32,6 +35,17 @@ class MessageCubit extends Cubit<MessageState> {
       (List<Message> messages) {
         allMessages = messages;
         emit(MessagesLoaded(messages));
+      },
+    );
+  }
+
+  Future<void> makeMessagesRead(String userId) async {
+    emit(MessageLoading());
+    final result = await _makeMessagesRead(userId);
+    result.fold(
+      (Failure failure) => emit(MessageError(failure.message)),
+      (_) {
+        emit(MessageSent());
       },
     );
   }
