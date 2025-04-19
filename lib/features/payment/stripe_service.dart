f53a3f86e0faa62700f3029c7fd0c85ff25fbd34
shroida <shroida012@gmail.com>
2025-04-19 17:13:29 +0200

    StripeService done

--- lib/features/payment/stripe_service.dart
+++ lib/features/payment/stripe_service.dart
@@ -1,1 +1,69 @@
-class StripeService {}
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class StripeService {
+  static const String _publishableKey = 'pk_test_TYooMQauvdEDq54NiTphI7jx';
+  static const String _secretKey = 'sk_test_BQokikJOvBiI2HlWgH4olfQ2';
+
+  // Initialize Stripe
+  static Future<void> init() async {
+    Stripe.publishableKey = _publishableKey;
+    Stripe.merchantIdentifier = 'merchant.com.yourapp';
+    await Stripe.instance.applySettings();
+  }
+
+  static Future<Map<String, dynamic>> createPaymentIntent(
+      {required String amount, required String currency}) async {
+    try {
+      final url = Uri.parse('https://api.stripe.com/v1/payment_intents');
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $_secretKey',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+          'amount': amount, // amount in cents (e.g., 10.00 = 1000)
+          'currency': currency,
+          'payment_method_types[]': 'card',
+        },
+      );
+
+      return json.decode(response.body);
+    } catch (err) {
+      throw Exception('Error creating PaymentIntent: $err');
+    }
+  }
+
+  // Initialize payment sheet
+  static Future<void> initPaymentSheet({
+    required String clientSecret,
+    required String customerId,
+    required String ephemeralKey,
+  }) async {
+    try {
+      await Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+          paymentIntentClientSecret: clientSecret,
+          merchantDisplayName: 'Your App Name',
+          customerId: customerId,
+          customerEphemeralKeySecret: ephemeralKey,
+          style: ThemeMode.system,
+        ),
+      );
+    } catch (e) {
+      throw Exception('Failed to initialize payment sheet: $e');
+    }
+  }
+
+  // Display the payment sheet
+  static Future<void> displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet();
+    } on Exception catch (e) {
+      throw Exception('Payment failed: $e');
+    }
+  }
+}
